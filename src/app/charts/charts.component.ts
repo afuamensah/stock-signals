d534a0dc60fe98fb6429424774edb33c7f74776d
Negar Jafari <54953465+nejafari@users.noreply.github.com>
2020-11-13 00:20:55 -0600

    Changes made

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-charts',
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.css']
 })
-export class AppComponent implements OnInit {
-  // imgSrc1: string;
-  // imgSrc2: string;
-  // imgSrc3: string;
-
+export class ChartsComponent implements OnInit {
 
   signals = [
     {sName: 'GBP/USD', rate: '(+0.02%)', slName:'British Pound/US Dollar', price: 1.2860, img: 'https://www.tradingview.com/x/q6VM9yQ4/'},
@@ -19,8 +15,5 @@ export class AppComponent implements OnInit {
 
   constructor() { }
   ngOnInit(): void {
-    // this.imgSrc1 = 'https://www.tradingview.com/x/q6VM9yQ4/'
-    // this.imgSrc2 = 'https://www.tradingview.com/x/XxvkuUZO/'
-    // this.imgSrc3 = 'https://www.tradingview.com/x/gMlAwT0o/'
   }
 }
