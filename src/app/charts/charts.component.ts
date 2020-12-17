7bee0748bc59c49e530e3e4c0ef7704febf932a9
Negar Jafari <54953465+nejafari@users.noreply.github.com>
2020-12-16 20:36:44 -0600

    Icons added

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ChartsComponent implements OnInit {
 
   signals = [
-    {sName: 'GBP/USD', rate: '(+0.02%)', slName:'British Pound/US Dollar', price: 1.2860, img: 'https://www.tradingview.com/x/q6VM9yQ4/'},
-    {sName: 'EUR/USD', rate: '(+0.07%)', slName:'Euro/US Dollar', price: 1.17475, img: 'https://www.tradingview.com/x/XxvkuUZO/'},
-    {sName: 'AUD/USD', rate: '(+0.09%)', slName:'Australian Dollar/US Dollar', price: 0.71343, img: 'https://www.tradingview.com/x/gMlAwT0o/'}
+    {flagImage: 'https://img.icons8.com/color/48/000000/great-britain-circular.png', sName: 'GBP/USD', rate: '(+0.02%)', slName:'British Pound/US Dollar', price: 1.2860, img: 'https://www.tradingview.com/x/q6VM9yQ4/'},
+    {flagImage: 'https://img.icons8.com/color/48/000000/japan-circular.png', sName: 'EUR/JPY', rate: '(+0.07%)', slName:'US Dollar/Japanese Yen', price: 1.17475, img: 'https://www.tradingview.com/x/mtUyV3A7/'},
+    {flagImage: 'https://img.icons8.com/color/48/000000/australia-circular.png', sName: 'AUD/USD', rate: '(+0.09%)', slName:'Australian Dollar/US Dollar', price: 0.71343, img: 'https://www.tradingview.com/x/gMlAwT0o/'}
   ];
 
   constructor() { }
--- src/app/signals/mock-signals.ts
+++ src/app/signals/mock-signals.ts
@@ -1,7 +1,7 @@
 import { Signal } from './signals';
 
 export const SIGNALS: Signal[] = [
-  {id: 1, sName: 'GBP/USD', rate: '(+0.02%)', slName:'British Pound/US Dollar', price: 1.2860, open: 1.286, high: 1.287, low: 1.286, show: true},
-  {id: 2, sName: 'EUR/USD', rate: '(+0.07%)', slName:'Euro/US Dollar', price: 1.175, open: 1.175, high: 1.175, low: 1.173, show: false},
-  {id: 3, sName: 'AUD/USD', rate: '(+0.09%)', slName:'Australian Dollar/US Dollar', price: 0.713, open: 0.713, high: 0.714, low: 0.713, show: false}
+  {id: 1, flagImage: 'https://img.icons8.com/color/48/000000/great-britain-circular.png', sName: 'GBP/USD', rate: '(+0.02%)', slName:'British Pound/US Dollar', price: 1.2860},, open: 1.286, high: 1.287, low: 1.286, show: true},
+  {id: 2, flagImage: 'https://img.icons8.com/color/48/000000/japan-circular.png', sName: 'EUR/JPY', rate: '(+0.07%)', slName:'US Dollar/Japanese Yen', price: 1.17475, open: 1.175, high: 1.175, low: 1.173, show: false},
+  {id: 3, flagImage: 'https://img.icons8.com/color/48/000000/australia-circular.png', sName: 'AUD/USD', rate: '(+0.09%)', slName:'Australian Dollar/US Dollar', price: 0.71343, open: 0.713, high: 0.714, low: 0.713, show: false}
 ];
